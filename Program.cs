ac3241077620972bb1700759de3d490f927dd711
Pavel Valenta <p.valenta@outlook.com>
2021-09-10 17:48:28 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogShovel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
